2ba87a7964e42cca91b258dca332c089a6a070f0
Arjun Patel <86199509+Arjuntina@users.noreply.github.com>
2024-01-10 06:56:55 -0800

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -3,14 +3,21 @@
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
   ];
+
+  # Kernel settings
+  boot.kernel.sysctl = { "vm.swappiness" = 10;}; # slider from 1-100 which determines how "agressively" to use swap (default = 60 on most distros)
+
+  # boot menu!
   boot.loader.systemd-boot.enable = false; # want to boot with GRUB
   boot.loader.grub.device = nodev;
   boot.loader.grub.efiSupport = true;
 
-  networking.networkmanager.enable = true; # for wifi
+  # wifi stuff
+  networking.networkmanager.enable = true;
   networking.hostName = "arjunslaptop";
 
   # Upgrading nix
+  # What way can I do this manually?
   system.autoUpgrade.enable = true;
   system.autoUpgrade.allowReboot = true; # is this necessary? Does it reboot randomly? hm hm
   
@@ -19,7 +26,7 @@
   # automatically in hardware-configuration.nix.
   #fileSystems."/".device = "/dev/disk/by-label/nixos";
 
-  # Enable the OpenSSH server.
+  # Enable the OpenSSH server. Not really sure?
   services.sshd.enable = true;
   # Maybe this as well?
   services.openssh.enable = true;
@@ -29,6 +36,14 @@
     vim
   ];
 
+  # Automatic Garbage Collection
+  # What way can I do this manually?
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
+
   users.users.arjuntina = {
     isNormalUser = true;
     description = "Arjun personal acct";
@@ -37,7 +52,7 @@
       git
       neovim
       firefox
-      zoom-us
+      zoom
     ];
   };
 }
