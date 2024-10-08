868aea2cd89624a308b23de3cca465974e8f2e0c
Arjun Patel <nujra911@gmail.com>
2024-10-07 23:48:07 -0400

    Updated for 2nd try with nix

--- configuration.nix
+++ configuration.nix
@@ -1,145 +1,214 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
+# The organization of this document is incomplete -- hopefully I can make it cooler/better in the future!
 
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
+
+  # NIX STUFF
+  # tbh i'm still learning this so its kinda messy rn
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
 
-  # Kernel settings
-  # boot.kernel.sysctl = {"vm.swappiness" = 10;}; # slider from 1-100 which determines how "aggressively" to use swap (default = 60 on most distros)
 
-  # Bootloader.
+
+  # HARDWARE STUFF
+  # Ofc not real hardware (that is in hardware-configuration) but this is for the input devices configuration information
+  
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # hardware.pulseaudio.enable = true;
+  # OR
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  services.libinput.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb.layout = "us";
+  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+
+
+
+  # BACKGROUND PROCESSES STUFF
+  # lower level things that form the "base" of the system
+
+  # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  # switch to grub options? maybe try switching on VM or other machine to test hm hm
-  # boot.loader.systemd-boot.enable = false;
-  # boot.loader.grub.device = nodev;
-  # boot.loader.grub.efiSupport = true;
 
-  # Upgrading nix
-  # is there a way I can do this manually>
-  # system.autoUpgrade.enable = true;
-  # system.autoUpgrade.allowReboot = true; # is this necessary? Does it reboot randomly? hm hm
-
-
+  # Networks
   networking.hostName = "arjunslaptop"; # Define your hostname.
+  # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager = {
+  	enable = true;  # Easiest to use and most distros use this by default.
+	dns = "default";
+	wifi.powersave = false;
+  };
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Enable networking
-  networking.networkmanager.enable = true;
 
   # Set your time zone.
   time.timeZone = "America/New_York";
-  # services.automatic-timezoned.enable = true; -- does automatically? hm hm
+
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkb.options in tty.
+  # };
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
+  
+
+  # DISPLAY + VISUAL STUFF
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  # services.xserver.desktopManager.plasma6.enable = true;
+  # Enable display/login manager
+  services.displayManager.sddm.enable = true;
 
-  # Enable window managers (qtile)
+  # Desktop environment
+  # services.xserver.desktopManager.gnome.enable = true;
+  services.desktopManager.plasma6.enable = true;
   services.xserver.windowManager.qtile.enable = true;
+  
 
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "";
-  };
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  # USER ACCOUNT AND PACKAGES
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.arjunslaptop = {
+  users.users.arjuntina = {
     isNormalUser = true;
-    description = "Arjun personal acct";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      # text editor
+    initialPassword = "123";
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+
+    # Packages for download
+    packages = with pkgs;
+    let
+      RStudio-with-my-packages = rstudioWrapper.override{
+        packages = with rPackages; [ 
+	  devtools
+	  curl
+	  knitr
+	  markdown
+	  tidyverse
+	  gt
+	  ggthemes
+	  shinylive
+	];
+      };
+    in
+    [
+      # Text Editor
       neovim
-      # background processes
-      wl-clipboard
+
       # CLI programs
-      git
       htop
-      # GUI applications
+
+      # Terminal
+      (pkgs.python3.withPackages (python-pkgs: [
+        python312Packages.tkinter
+      ]))
+      alacritty
+
+      # Launchers (add dmenu one day maybe)
+      rofi-wayland
+
+      # Documents??
+      texliveBasic
+      pandoc
+
+      # GUI Programs
+      ## Web browsers
       firefox
-      obsidian
-      anki
-      libreoffice
-      stellarium
+      ## Communication
+      thunderbird
       discord
+      zoom-us
+      ## Documents
+      libreoffice-qt
+      obsidian
       inkscape
       gimp
-      vscodium
-      zathura
       blender
-      zoom-us
-      chromium
+      zathura
+      ## Coding
+      RStudio-with-my-packages
+      vscodium
+      ## Learning
+      anki-bin
+      ## Sciency
+      stellarium
+      ## Virtual Machine
+      virtualbox
+      ## Utility
+      flameshot
       cinnamon.nemo
-      samba
-      alacritty
+      ## Gaming
+      (retroarch.override {
+        cores = with libretro; [
+	  bsnes
+        ];
+      })
+
+      # What is this?
+      tree
+
     ];
   };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-  nixpkgs.config.permittedInsecurePackages = [
-    "electron-25.9.0" # sigh i'm not sure why but apparently electron is EOL -- hopefully obsidian package updates in future
-  ];
+  programs.steam.enable = true;
+  programs.steam.gamescopeSession.enable = true;
+  programs.gamemode.enable = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
+    git
   ];
+
+  nixpkgs.config.allowUnfree = true;
+
+  fonts.packages = with pkgs; [
+    fira-code-nerdfont
+    font-awesome
+  ];
+
+
+  # OTHER STUFF THAT I DON'T REALLY UNDERSTAND AND NEED/WANT TO FIGURE OUT
+
+  # Sort out bc its getting a little messy rn
+  # But this is for apple laptop webcam
+  hardware.facetimehd.enable = true;
+
+  # Hyprland
+  # gosh I really need to clean up the config
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -152,7 +221,7 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+  # services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
@@ -160,12 +229,29 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "24.11"; # Did you read the comment?
 
 }
+
