6524fa5a2ba1175870fcc4a64098ae53dcbbc2b0
Arjun Patel <nujra911@gmail.com>
2024-12-11 15:12:56 -0500

    Added configuration to mpv to make it open in a large screen

--- programs/mpv/mpvConfig.nix
+++ programs/mpv/mpvConfig.nix
@@ -16,6 +16,9 @@
             config = {
                 keep-open="always";  # prevent mpv from closing and from going to the next entry in a playlist automatically
 
+                # Make the videos always open at 70% of the width of the screen
+                autofit="70%";
+
                 # For modernx UI
                 osc="no";
                 border="no";
