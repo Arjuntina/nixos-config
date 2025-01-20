7615805756b47d0da70b35b86d6070597759f0f5
Arjun Patel <nujra911@gmail.com>
2025-01-20 13:44:23 -0800

    Changed timezone to EST

--- configuration.nix
+++ configuration.nix
@@ -113,7 +113,7 @@
 		# networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
 		# Set your time zone.
-		time.timeZone = "America/Los_Angeles";
+		time.timeZone = "America/New_York";
 
 		# Select internationalisation properties.
 		i18n.defaultLocale = "en_US.UTF-8";
