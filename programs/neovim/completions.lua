e6e3c6f481c329072009172fa937c3bea41ffeef
Arjun Patel <nujra911@gmail.com>
2025-01-30 18:59:06 -0500

    Adding and learning snippet configurations luasnip

--- programs/neovim/completions.lua
+++ programs/neovim/completions.lua
@@ -1,8 +1,8 @@
--- Everything from this section was gleaned from the typecraft vids + github pages
+-- cmp configuration
+-- cmp is the "engine" that allows us to see and expand snippets
+-- Configuration here is based on typecraft vids + github pages 
+-- It will use luasnip as a backend
 local cmp = require'cmp'
-
--- The line below lets us use "friendly snippets" from vscode (in documentation for luasnip)
-require("luasnip.loaders.from_vscode").lazy_load()
 
 -- The setup function below comes from the documentation for nvim-cmp
 cmp.setup({
@@ -33,11 +33,45 @@ cmp.setup({
         { name = 'buffer' },
     })
 })
--- end section
 
 
--- Everything in this section was gleaned from math latex articles
--- Will set up completions later on (future project) as will take a while
+
+-- Luasnip configuration
+-- the snippet expansion completion engine
+-- I have configured luasnip to use both manual snippets (which am learning) as well as vscode snippets (which I copied from typecraft vids)
+
+-- These variables (there are a lot) are apparently very useful to define for configuration -- are taken from the luasnip documentation/githib & a lot of other people use them
+local ls = require("luasnip")
+local s = ls.snippet
+local sn = ls.snippet_node
+local isn = ls.indent_snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local c = ls.choice_node
+local d = ls.dynamic_node
+local r = ls.restore_node
+local events = require("luasnip.util.events")
+local ai = require("luasnip.nodes.absolute_indexer")
+local extras = require("luasnip.extras")
+local l = extras.lambda
+local rep = extras.rep
+local p = extras.partial
+local m = extras.match
+local n = extras.nonempty
+local dl = extras.dynamic_lambda
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+local conds = require("luasnip.extras.expand_conditions")
+local postfix = require("luasnip.extras.postfix").postfix
+local types = require("luasnip.util.types")
+local parse = require("luasnip.util.parser").parse_snippet
+local ms = ls.multi_snippet
+local k = require("luasnip.nodes.key_indexer").new_key
+
+
+-- The code below allows tab & shift-tab to expand/unexpand snippets?
+-- Not sure of the exact specifics - could be something to look into!
 vim.cmd[[
     " Use Tab to expand and jump through snippets
     imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' 
@@ -48,5 +82,66 @@ vim.cmd[[
     smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
 ]]
 
+-- The code below allows certain snippets to be autoexpandable
+ls.config.setup({ enable_autosnippets = true })
 
--- end section
+-- The configuration below here allows moving across different input fields within a snippet
+-- am using <A-j> for forward and <A-k> for backward (vim down & up)
+vim.keymap.set({ "i", "s" }, "<A-j>", function()
+    if ls.expand_or_jumpable() then
+        ls.expand_or_jump()
+    end
+end, { silent = true })
+
+vim.keymap.set({ "i", "s" }, "<A-k>", function()
+    if ls.jumpable(-1) then
+        ls.jump(-1)
+    end
+end, { silent = true })
+
+-- The line below lets us use "friendly snippets" from vscode (in documentation for luasnip)
+-- copied from typecraft vids
+require("luasnip.loaders.from_vscode").lazy_load()
+
+-- The configuration below contains manual snippet expansions
+-- am still learning how it works
+-- snippets for all files (used mainly for testing purposes)
+ls.add_snippets("all", {
+})
+-- snippets for lua files (also mostly for testing)
+ls.add_snippets("lua", {
+    s("hello", {
+        t('print("hello world")')
+    })
+})
+-- snippets for latex files
+ls.add_snippets("tex", {
+    -- Overall:
+    -- Sections
+    s({ trig = "sec", -- short for "section"
+        snippetType = "autosnippet", -- allows for the snippet to autoexpand
+         -- can only expand the snippet at the beginning of a line
+        name = "Create Latex section",
+    },{
+        t("\\begin{"), i(1), t("}"),
+        t({ "", "\t" }), i(0), -- strange that i(0) is used here but i(0) refers to the last input
+        t({ "", "\\end{" }), rep(1), t("}"),
+    }),
+
+    -- Math:
+    -- fractions
+    s("frac", {
+        t("\\frac{"),
+        i(1),
+        t("}{"),
+        i(2),
+        t("}")
+    }),
+    -- text
+    s("text", {
+        t("\\text{"),
+        i(1),
+        t("}")
+    }),
+})
+
--- programs/neovim/plugins/vimtex.lua
+++ programs/neovim/plugins/vimtex.lua
@@ -11,6 +11,7 @@ vim.g.maplocalleader = "\\"
 
 -- Documentation in the section below is from good online articles
 -- The commands below conceals $ and [ latex brackets until you enter the line which they are on, making the document cleaner -- also they replace math symbols (investigate further)
+-- They do not seem to work :(
 vim.cmd("set conceallevel=1")
 vim.g.text_conceal="abdmg"
 
