73730d0f5bceff32f6581e6cbb80cb0ad6b15ff6
Arjun Patel <nujra911@gmail.com>
2025-05-24 12:22:09 -0400

    Added Super+P / Super+Shift+P keybinds to allow for easy potential screenshot editing

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -5,8 +5,8 @@
 # Intended Functionality:
 # Take a screenshot of a selected area on screen and copy it to the clipboard on hyprland!
 # Features I want:
-# The ability to edit the screenshot
-# The ability to save the screenshot to a file, if needed
+# The ability to edit the screenshot, if I want to
+# The ability to save the screenshot to a file, if I want to
 
 # Explanation of key programs used:
 # Grim: captures an entire screen or part of a screen rendered by wayland
@@ -18,10 +18,30 @@
 # wl-clipboard: a cli utility that allows users to send items to the system clipboard and retrieve items from the system clipboard
 #               the system clipboard is built into wayland, but cannot be accessed without this special tool
 
-# grim -g "$(slurp)" - takes a screenshot and sends it to stdout
-# swappy -f - opens it for annotation
-# After annotation, saeit to a temp file
-# wl-copy to put the file on my clipboard
-grim -g "$(slurp)" - | swappy -f - -o /tmp/swappy-output.png && wl-copy < /tmp/swappy-output.png
+# Have multiple functionalities depending on the input passed into the program
+case "$1" in
+    1)
+        # If input = 1, take screenshot from mouse selection & annotate the screenshot with swappy
+        # grim -g "$(slurp)" - takes a screenshot and sends it to stdout
+        # swappy -f - opens it for annotation
+        # After annotation, saeit to a temp file
+        # wl-copy to put the file on my clipboard
+        grim -g "$(slurp)" - | swappy -f - -o /tmp/swappy-output.png && wl-copy < /tmp/swappy-output.png
+        ;;
+    2)
+        # If input = 2, take screenshot from mouse selection but do NOT use swappy to annotate screenshot
+        # grim -g "$(slurp)" - takes a screenshot and sends it to stdout
+        # wl-copy to immediately copy the screenshot to the clipboard
+        grim -g "$(slurp)" - | wl-copy
+        ;;
+    *)
+        # If no input is provided, display some help info (copied from chatgpt)
+        echo "Usage: $0 [1|2]"
+        echo "1 - Select region, annotate, copy to clipboard"
+        echo "2 - Select region, copy to clipboard (no annotation)"
+        ;;
+esac
+
 
 # To-do: customize swappy (can customize some aspects of the software & stuff like what directory to save files to)
+# To-do: use zsh?
