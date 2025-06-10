f9f9ba9abe987944cfc6c969ed609c8cd15a16ce
Arjun Patel <nujra911@gmail.com>
2025-06-10 01:40:14 -0400

    Attenpted to switch to nvidia driver - if successful will have to document and learn still though bc am still quite clueless and just copying code for now

--- configuration.nix
+++ configuration.nix
@@ -79,15 +79,15 @@
 			enable = true;
 		};
 		# Load nvidia driver for Xorg and Wayland
-		#services.xserver.videoDrivers = ["nvidia"];
-		#hardware.nvidia = {
+		services.xserver.videoDrivers = ["nvidia"];
+		hardware.nvidia = {
 			# Modesetting is required.
-			#modesetting.enable = true;
+			modesetting.enable = true;
 			# Nvidia power management. Experimental, and can cause sleep/suspend to fail.
 			# Enable this if you have graphical corruption issues or application crashes after waking
 			# up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
 			# of just the bare essentials.
-			#powerManagement.enable = false;
+			powerManagement.enable = true;
 			# Fine-grained power management. Turns off GPU when not in use.
 			# Experimental and only works on modern Nvidia GPUs (Turing or newer).
 			#powerManagement.finegrained = false;
@@ -104,7 +104,11 @@
 			#nvidiaSettings = true;
 			# Optionally, you may need to select the appropriate driver version for your specific GPU.
 			#package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
-		#};
+
+            # Apparently the closed source is better
+            open = false;
+		};
+        boot.blacklistedKernelModules = [ "nouveau" ];
 
 
 
